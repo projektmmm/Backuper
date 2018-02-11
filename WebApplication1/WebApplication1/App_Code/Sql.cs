79f18f14c173df4c52a94117ff5b9315a310e78f
Vojtech Muller <mueller.vojtech@gmail.com>
2018-02-11 22:48:03 +0100

    rest api
    Priklad pouziti rest api. Podklad pro konzolovku

---
+++ WebApplication1/WebApplication1/App_Code/Sql.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public static class Sql
+    {
+        public static string ConnectionString = "Data Source = VOJTAPC\\VSQL;" + "Initial Catalog = CORE;" + "User ID = CERIS;" + "Password = Alpiq123456";
+        public static SqlConnection sConn = new SqlConnection(ConnectionString);
+        public static string query = "SELECT * FROM [CORE].[dbo].[V_BATCH_SCHEDULER]";
+        public static SqlCommand sComm = new SqlCommand(query, sConn);
+
+        public static void SetCommand(string query)
+        {
+            Sql.query = query;
+            Sql.sComm = new SqlCommand(Sql.query, Sql.sConn);
+        }
+
+        public static void Open()
+        {
+            sConn.Close();
+            sConn.Open();
+        }
+
+        public static void Close()
+        {
+            sConn.Close();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonController : ApiController
+    {
+
+        //Api/Person
+        public IEnumerable<Person> Get()
+        {
+            return this.FindAll();
+        }
+
+        public Person Get(int id)
+        {
+            return this.FindAll().Where(x => x.Id == id).First();
+        }
+
+        public void Post([FromBody]Person person)
+        {
+            int a = 0;
+        }
+
+
+        private List<Person> FindAll()
+        {
+            return new List<Person>()
+            {
+                new Person() { Id = 1, Name = "Pepa", Surname = "Novak", Age = 52 },
+                new Person() { Id = 2, Name = "Karel", Surname = "Král", Age = 52 },
+                new Person() { Id = 3, Name = "Viktor", Surname = "Lusk", Age = 52 },
+            };
+        }
+    }
+}
