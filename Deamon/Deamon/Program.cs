13712974770edcf086f4d3cacb3453198bbd6903
projektmmm <musilekvojtech@sssvt.cz>
2018-02-11 20:03:59 +0100

    plz
    funguj

---
+++ Deamon/Deamon/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deamon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Backuper b = new Backuper();
+
+            b.FullBackup();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Deamon/Deamon/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deamon
+{
+    public static class Settings
+    {
+        public static string SourcePath { get; set; } = @"D:\ProjektMMM\From\";
+        public static string DestinationPath { get; set; } = @"D:\ProjektMMM\FullBackup\";
+    }
+}
