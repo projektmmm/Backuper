3280455590cd31051f27f2e041e4908e453b263f
Vojtech Muller <mueller.vojtech@gmail.com>
2018-02-15 14:03:35 +0100

    Daemon, rozpracovane API

---
+++ Deamon/Deamon/ApiCommunication.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Deamon
+{
+    public class ApiCommunication
+    {
+        public List<FileInfo> ToPost { get; set; }
+
+        public async void Post()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = Connection.Api;
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage Response = await client.PostAsJsonAsync("api/person", this.ToPost);
+            }
+        }
+    }
+}
