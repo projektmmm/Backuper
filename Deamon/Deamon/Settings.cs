0d9450ba168d04bcfac35e1acf8ddc4ff145b947
Vojtech Muller <mueller.vojtech@gmail.com>
2018-02-17 13:43:26 +0100

    Rozsireni databaze

--- Deamon/Deamon/Settings.cs
+++ Deamon/Deamon/Settings.cs
@@ -8,6 +8,8 @@ namespace Daemon
 {
     public static class Settings
     { 
+        public static string DaemonId { get; private set; }
+        public static DateTime RunAt { get; set; }
         public static string SourcePath { get; set; } = @"C:\ProjektMMM\From\";
         public static string DestinationPath { get; set; } = @"C:\ProjektMMM\To\";
     }
