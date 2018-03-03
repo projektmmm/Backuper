99a7d18d9b5091affab647eee9b2deb9d059a3d8
Vojtech Muller <mueller.vojtech@gmail.com>
2018-03-03 19:46:09 +0100

    Uprava formatu

--- Deamon/Deamon/Backups/BackupMenu.cs
+++ Deamon/Deamon/Backups/BackupMenu.cs
@@ -12,41 +12,25 @@ namespace Daemon
     {
         public IBackup Back;
 
-<<<<<<< HEAD
-        public void ChangeBackupSettings(int type)
-=======
-        //Tuhle classu využijeme jako středobod backupů. Musíme vymyslet nějáký chytrý pusob jak je přepínat
-        //FullBackup fb = new FullBackup();
-        //DifferentialBackup df = new DifferentialBackup();
-
-
+    
         int BackupType = 1; //1-Full,2=Diff
         public string SourcePath { get; set; } = @"C:\ProjektMMM\From\";
         public string DestinationPath { get; set; } = @"C:\ProjektMMM\To\";
 
-        public void ChangeType(int type,string sorcePath,string destinationPath)
->>>>>>> 75d14c7ecd5aa9a38f65215afa279373b46a1a8f
+        public void ChangeType(int type, string sourcePath, string destinationPath)
         {
             if (type ==1)
             {
-<<<<<<< HEAD
-                this.Back = new FullBackup();
-=======
-                //this.fb.Backup(this.SourcePath,this.DestinationPath);
->>>>>>> 75d14c7ecd5aa9a38f65215afa279373b46a1a8f
+                this.Back = new FullBackup(sourcePath, destinationPath);
             }
             else if(type ==2)
             {
-<<<<<<< HEAD
-                this.Back = new DifferentialBackup();
-=======
-                //this.df.Backup(this.SourcePath, this.DestinationPath);
->>>>>>> 75d14c7ecd5aa9a38f65215afa279373b46a1a8f
+                this.Back = new DifferentialBackup(sourcePath, destinationPath);
             }
         }
         public void StartBackup(string sourcePath, string destinationPath)
         {
-            this.Back.Backup(sourcePath,destinationPath);
+            this.Back.Backup();
         }
     
 
