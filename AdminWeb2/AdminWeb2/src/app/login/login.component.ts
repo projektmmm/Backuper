b721fe1ef1f0bfef1e430d860711c239f1cc3eb3
MacekDa <36379751+MacekDa@users.noreply.github.com>
2018-03-29 18:13:35 +0200

    Login for Adminweb2

--- AdminWeb2/AdminWeb2/src/app/login/login.component.ts
+++ AdminWeb2/AdminWeb2/src/app/login/login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClientModule, HttpClient, HttpParams,HttpClientJsonpModule,HttpHeaders} from '@angular/common/http';
+import { User } from './User';
+import { RouterLink, Router } from '@angular/router';
 @Component({
-  selector: 'login',
+  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+    
+    constructor(private http :HttpClient) {
+     
+  }
+  Login(IUsername:string,IPassword:string) {
+    const head =  {headers: new  HttpHeaders({'Content-Type':'application/json'}) };
+    head.headers.append('Content-Type', 'application/json')
+    const data: User={
+      Username:IUsername,
+      Password:IPassword
+    }
+    this.http.post<boolean>('http://localhost:54736/api/admin/login',data,head)
+    .subscribe(Response=>{
+      if(Response)
+      {
+        localStorage.setItem("Data",JSON.stringify(Response));
+        console.log("Done");
+      }
+      else{
+        console.log("Nope")
+      }
+    }
+    )}
+  Register(){
+    
+  }
   ngOnInit() {
+    
   }
 
 }
--- AdminWeb2/AdminWeb2/src/app/register/register.component.ts
+++ AdminWeb2/AdminWeb2/src/app/register/register.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClientModule, HttpClient, HttpParams,HttpClientJsonpModule,HttpHeaders} from '@angular/common/http';
+import { User } from './User';
+import { RouterLink, Router } from '@angular/router';
 @Component({
-  selector: 'register',
+  selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http :HttpClient) { 
 
+  }
   ngOnInit() {
+    
   }
 
-}
+  Register(IUsername:string,IPassword:string,IEmail:string){
+    const head =  {headers: new  HttpHeaders({'Content-Type':'application/json'}) };
+    head.headers.append('Content-Type', 'application/json')
+    const data: User={
+      Username:IUsername,
+      Password:IPassword,
+      Email:IEmail
+    }
+    this.http.post<boolean>('http://localhost:54736/api/admin/register',data,head)
+    .subscribe(Response=>{
+      if(Response)
+      {
+        console.log("Succ");
+      }
+      else{
+        console.log("XXX")
+      }
+    }
+    )}
+  }
+  
+
+
