4efcbdb94c2e88d6105e997916b6e25456212fd9
vojtechmusilek <vojtechmusilekk@gmail.com>
2018-06-03 19:39:11 +0200

    mám

--- AdminWeb2/AdminWeb2/src/app/send-new-settings/settings.ts
+++ AdminWeb2/AdminWeb2/src/app/send-new-settings/settings.ts
@@ -3,7 +3,15 @@ export interface Settings {
     Cron: string;
     BackupType: string;
     SourcePath: string;
-    DestinationPath: string
+    DestinationPath: string;
+    FTPServerAdress: string;
+    FTPPort: string;
+    FTPUsername: string;
+    FTPPassword: string;
+    SSHServerAdress: string;
+    SSHPort: string;
+    SSHUsername: string;
+    SSHPassword: string;
 
     Id: number;
 }
